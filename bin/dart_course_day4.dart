f62a2b1164118b937f1ceedd252c071836f4d19c
Ahmad <134114334+AhmadXDev@users.noreply.github.com>
2024-12-04 16:30:28 +0300

    first commit

---
+++ bin/dart_course_day4.dart
@@ -0,0 +1,5 @@
+import 'package:dart_course_day4/dart_course_day4.dart' as dart_course_day4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_course_day4.calculate()}!');
+}
---
+++ test/dart_course_day4_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_course_day4/dart_course_day4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
